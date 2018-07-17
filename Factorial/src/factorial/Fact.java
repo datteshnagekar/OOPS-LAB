7cc43269f09dbaae1fb24811fe4b02b964a0a6b1
datteshnagekar <datteshnagekar7667@gmail.com>
2018-07-17 16:15:31 +0530

    initial commit factorial

---
+++ Factorial/src/factorial/Fact.java
@@ -0,0 +1,21 @@
+package factorial;
+
+public class Fact {
+
+	public static void main(String[] args)//
+	{
+		 Integer f=4,i=f,n;
+		 
+		 	
+		 	System.out.println("to find the factorial of a number ok");
+		 	
+			for(n=1; n<i; n++)
+			{
+				f=f*n;
+				System.out.println(f);
+				
+			}
+			System.out.println("the factorial of the number is ok  "  +f);
+		
+	}
+}
